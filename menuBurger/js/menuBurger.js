53729865a6610574a84dbff1198ee6f692f5ddfa
Dev-IV-Dnl <yduquesnel.pro@gmail.com>
2021-11-03 16:42:24 +0100

    maj burger menu et header

--- menuBurger/js/menuBurger.js
+++ menuBurger/js/menuBurger.js
@@ -18,13 +18,11 @@ burgerMenu.addEventListener("click", () => {
     console.log("Je clique sur le menu Burger");
     if (menuDown.style.height === "200px") {
         menuDown.style.height = "0";
-        menuDown.style.border = "none";
         ligne1Burger.style.transform = "rotate(0deg)";
         ligne2Burger.style.backgroundColor = "var(--col1)";
         ligne3Burger.style.transform = "rotate(0deg)";
     } else if (menuDown.style.height === "0px") {
         menuDown.style.height = "200px";
-        menuDown.style.border = "1px solid var(--col1)";
         ligne2Burger.style.backgroundColor = "transparent";
         ligne1Burger.style.transform = "translate(4px, 1px) rotate(40deg)";
         ligne3Burger.style.transform = "translate(4px, 0px) rotate(-40deg)";
