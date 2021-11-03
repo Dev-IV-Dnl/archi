b83261b2bbe0dcc304aabd347a0e4b6c569c05dd
Dev-IV-Dnl <yduquesnel.pro@gmail.com>
2021-11-03 15:01:17 +0100

    maj lignes menu burger

--- menuBurger/js/menuBurger.js
+++ menuBurger/js/menuBurger.js
@@ -8,9 +8,9 @@
 
 let menuDown = document.querySelector("#menuDown");
 let burgerMenu = document.querySelector("#iconeMenuBurger");
-let ligne1 = document.querySelector(".ligne1");
-let ligne2 = document.querySelector(".ligne2");
-let ligne3 = document.querySelector(".ligne3");
+let ligne1Burger = document.querySelector(".ligne1Burger");
+let ligne2Burger = document.querySelector(".ligne2Burger");
+let ligne3Burger = document.querySelector(".ligne3Burger");
 
 menuDown.style.height = "0";
 menuDown.style.border = "none";
@@ -19,14 +19,14 @@ burgerMenu.addEventListener("click", () => {
     if (menuDown.style.height === "200px") {
         menuDown.style.height = "0";
         menuDown.style.border = "none";
-        ligne1.style.transform = "rotate(0deg)";
-        ligne2.style.backgroundColor = "var(--col1)";
-        ligne3.style.transform = "rotate(0deg)";
+        ligne1Burger.style.transform = "rotate(0deg)";
+        ligne2Burger.style.backgroundColor = "var(--col1)";
+        ligne3Burger.style.transform = "rotate(0deg)";
     } else if (menuDown.style.height === "0px") {
         menuDown.style.height = "200px";
         menuDown.style.border = "1px solid var(--col1)";
-        ligne2.style.backgroundColor = "transparent";
-        ligne1.style.transform = "translate(4px, 1px) rotate(40deg)";
-        ligne3.style.transform = "translate(4px, 0px) rotate(-40deg)";
+        ligne2Burger.style.backgroundColor = "transparent";
+        ligne1Burger.style.transform = "translate(4px, 1px) rotate(40deg)";
+        ligne3Burger.style.transform = "translate(4px, 0px) rotate(-40deg)";
     }
 })
